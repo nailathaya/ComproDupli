51d85c24d1a9811f5edcb995ab6db9ce2979fe07
nailathaya <athhayamumtaz@gmail.com>
2026-01-07 01:51:23 +0700

    m

--- ai-recruitment-be/main.py
+++ ai-recruitment-be/main.py
@@ -16,7 +16,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://ai-recruitment-app-production-942f.up.railway.app"
+        # "https://ai-recruitment-app-production-942f.up.railway.app"
+        "https://ai-recruitment-app-eight.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
