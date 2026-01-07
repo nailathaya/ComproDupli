0e72dea516c8086b84a9661a818c29229d4c984e
nailathaya <athhayamumtaz@gmail.com>
2026-01-07 21:14:36 +0700

    ubah api base url

--- ai-recruitment-fe-reupload/store/useHrdStore.ts
+++ ai-recruitment-fe-reupload/store/useHrdStore.ts
@@ -7,7 +7,7 @@ import {
   getJobPostings
 } from '../services/api';
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 /* =====================
    AUTH HEADER
