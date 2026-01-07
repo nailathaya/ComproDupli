c837c554212173c90d4e1719c24b1e37ea4ba433
nailathaya <athhayamumtaz@gmail.com>
2026-01-07 22:42:57 +0700

    bismillah

--- ai-recruitment-fe-reupload/pages/hrd/JobDetailPage.tsx
+++ ai-recruitment-fe-reupload/pages/hrd/JobDetailPage.tsx
@@ -204,7 +204,7 @@ import {
 import { format } from 'date-fns';
 import { id as idLocale } from 'date-fns/locale';
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const JobDetailPage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
